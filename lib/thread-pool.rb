138a75c8ffe93c9df7c1c7bf7a445d8cfbf9b73c
Evan Dowling <evandowling@Evan-Dowlings-MacBook-Pro.local>
2010-06-02 08:42:27 -0700

    first commit for thread pool gem

---
+++ lib/thread-pool.rb
@@ -0,0 +1,64 @@
+class ThreadPool
+  class << self
+    def start_up(thread_count)
+      @thread_work_queues = {}
+      @locks = {}
+      @threads = []
+      @current_jobs = {}
+      thread_count.times do |i|
+        @thread_work_queues[i] = []
+        @locks[i] = Mutex.new
+        @threads << Thread.new(i,@thread_work_queues[i],@locks[i],@current_jobs) do |index,queue,lock,executing_jobs|
+          
+          while true
+            if queue.size > 0
+              #do the mutex
+              job = nil
+              lock.synchronize { 
+                job = queue.slice!(0) 
+              }
+              if job
+                executing_jobs[index] = job
+                job[:block].call(job[:args])
+                executing_jobs.delete(index)
+              end
+              
+            end
+            #keep looking for work until the parent pid dies
+            #puts "Worker-#{index} out of stuff to do"
+            sleep(0.1)
+          end
+             
+        end
+      end
+    end
+    
+    def give_work( *args,&block)
+      min = -1
+      min_key = nil
+      @thread_work_queues.each_pair do |k,v|
+        if min_key == nil or v.size < min
+          min = v.size
+          min_key = k
+        end
+      end
+      @thread_work_queues[min_key]
+      @locks[min_key].synchronize {
+        @thread_work_queues[min_key] << {:block => block, :args => args}
+      }
+    end
+    
+    def queue_size
+      size = 0
+      @thread_work_queues.each_pair do |k,v|
+        size += v.size
+      end
+      size
+    end
+    
+    def num_executing
+      @current_jobs.keys.inject(0){|a,b| @current_jobs[b] ? a+ 1 : a }
+    end
+    
+  end
+end
---
+++ thread-pool.gemspec
@@ -0,0 +1,13 @@
+spec = Gem::Specification.new do |s| 
+  s.name = "thread-pool"
+  s.version = "0.0.1"
+  s.author = "Evan Dowling"
+  s.email = "dowling.evan@gmail.com"
+  s.homepage = "http://github.com/evandowling"
+  s.platform = Gem::Platform::RUBY
+  s.summary = "A gem to package thread pools in an easy to use way"
+  s.files = ["lib/thread-pool.rb"]
+  s.require_path = "lib"
+  s.has_rdoc = false
+  s.extra_rdoc_files = ["README"]
+end
